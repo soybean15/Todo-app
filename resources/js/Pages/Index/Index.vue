b5b90626fb820c1f4441730c0944291338c76738
soybean15 <marlonpadilla1593@gmail.com>
2024-11-05 15:08:47 +0800

    Update - inital commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+
+<template>
+
+<span class="text-red-400">Hello {{ counter }}!</span>
+
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  const counter = ref(0)
+  setInterval(() => counter.value++, 1000)
+</script>
